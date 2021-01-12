318f04e3af3b70cf6fccc1da3eccdbab829490a5
deepshikha-singh <deepshikha20k@gmail.com>
2021-01-12 18:11:55 +0530

    work on Talent Height

--- web/urls.py
+++ web/urls.py
@@ -6,30 +6,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.index, name='index' ),
-    path('show-category/', views.show, name='show' ),
-    path('movie-category/', views.movie, name='movie' ),
-    path('manage-profile/', views.manage, name='manage' ),
-    path('movie-details/', views.manage, name='details' ),
-    path('pricing-plan/', views.manage, name='pricing' ),
-    path('reset-password/', views.manage, name='reset' ),
-    path('setting/', views.manage, name='setting' ),
-    path('login/', views.manage, name='login' ),
-    path('show-details/', views.manage, name='s-details' ),
-    path('show-signle/', views.manage, name='signle' ),
-    path('show-single/', views.manage, name='single' ),
-    path('sign-up/', views.manage, name='sign-up' ),
-    path('watch-video/', views.manage, name='watch' ),
-    path('index-2/', views.manage, name='index-2' )
-
-
-
-
-
-
-
-
-
-
+    path('register-into-talentheight', views.signup, name='signup'),
+    path('login-into-talentheight', views.signin, name='signin'),
 
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- web/views.py
+++ web/views.py
@@ -1,47 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
+from django.http import HttpResponse, JsonResponse
+from django.conf import settings
+from .models import *
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
-def show(request):
-    return render(request, 'show-category.html')
-
-def movie(request):
-    return render(request, 'movie-category.html')
-
-def manage(request):
-    return render(request, 'manage-profile.html')
-
-def details(request):
-    return render(request, 'movie-details.html')
-
-def pricing(request):
-    return render(request, 'pricing-plan.html')
-
-def reset(request):
-    return render(request, 'reset-password.html')
-
-def setting(request):
-    return render(request, 'setting.html')
-
-def login(request):
-    return render(request, 'login.html')
-
-def sdetails(request):
-    return render(request, 's-details.html')
-
-def signle(request):
-    return render(request, 'signle.html')
-
-def single(request):
-    return render(request, 'single.html')
-
 def signup(request):
-    return render(request, 'sign-up.html')
+    if request.method == 'POST':
+        # Get the post parameters
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
 
-def watch(request):
-    return render(request, 'watch-video.html')
+        # Create New User
+        myuser = User.objects.create_user(username, email, password)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+        messages.success(request, "Your Talent Height account has been successfully created")
+        return redirect('index')
+    else:
+        return render(request, 'signup.html')
 
-def index2(request):
-    return render(request, 'index-2.html')
+def signin (request):
+    if request.method == 'POST':
+        #Get the post parameter
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        
+        if user is not None: 
+            messages.success(request, "Sucessfully Login into Kibtrip")
+            return redirect('index')
+        else:
+            messages.error(request, "Invalid Credentials!  Please try again")
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
+
+def log_out(request):
+    logout(request)
+    messages.success(request, "Successfully Logged Out")
+    
+    return redirect('index')
